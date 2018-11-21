e74ddfac6690d127d2315141c0970561fa9b72ba
Rohan Peri <perirohan@gmail.com>
2018-11-20 23:03:48 -0500

    This is all the work we did today. Great Start Omar.

---
+++ RobotCode/src/org/usfirst/frc/team4015/robot/subsystems/Drivebase.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team4015.robot.subsystems;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drivebase extends Subsystem {
+	private Talon LeftFrontMotor;
+	private Talon LeftBackMotor;
+	private Talon RightFrontMotor;
+	private Talon RightBackMotor;
+	
+	@Override
+	protected void initDefaultCommand() {
+		
+	}
+
+}
---
+++ RobotCode/src/org/usfirst/frc/team4015/robot/subsystems/joystickControl.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team4015.robot.subsystems;
+
+import org.usfirst.frc.team4015.robot.*;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+public class joystickControl extends Subsystem {
+
+	Joystick LeftJoystick = new Joystick(RobotMap.leftStickPort);
+	Joystick RightJoystick = new Joystick(RobotMap.rightStickPort);
+	
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+s
